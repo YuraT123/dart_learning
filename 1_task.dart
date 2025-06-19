4980b375c94ac5a5726b769f1fc31263a2e7a8f1
YuraT123 <tomishchak@gmail.com>
2025-06-19 21:40:56 +0300

    feat(DART-06): Math operators

--- 1_task.dart
+++ 1_task.dart
@@ -1,3 +1,13 @@
-void main(List<String> args) {
-  
+void main() {
+  double width = 18.4;
+  double length = 20.9;
+  double area = width * length;
+  double perimeter = 2 * (width + length);
+  int fullsquares =area~/1.0;
+
+  print("The perimeter of the rectangle is: $perimeter units");
+  print("The area of the rectangle is: $area square units");
+  print("Number of full squares that can fit in the rectangle: $fullsquares");
+
+
 }
