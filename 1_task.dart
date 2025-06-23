f12e1f86c3f5926658b99563724155ae817ad054
YuraT123 <tomishchak@gmail.com>
2025-06-23 18:46:17 +0300

    feat(DART-06):10 Break and Continue

--- 1_task.dart
+++ 1_task.dart
@@ -127,6 +127,14 @@ int number1 = 10;
         number1--;
   }
 
-
+for (int i = 1; i <= 20; i++) {
+       if (i % 3 == 0) {
+      continue;
+    }
+       if (i > 15) {
+      break;
+    }
+       print(i);
+  }
 
 }
